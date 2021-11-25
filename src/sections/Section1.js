26e4e3ccbd29722ca5941dc50f6669d5b6875464
ralph <smmvaralph@yahoo.com>
2021-11-25 05:36:59 -0800

    added text

--- src/sections/Section1.js
+++ src/sections/Section1.js
@@ -6,6 +6,7 @@ function Section1() {
             <div>
                 <h1>ReWORX</h1>
                 <p>Strategic Outsourcing, ReWORXed.</p>
+                <p>group project</p>
             </div>
         </div>
     )
