a1b331f04c78d413b79e0320be65daefee9a1c8e
ralph <smmvaralph@yahoo.com>
2021-11-28 03:26:34 -0800

    update4

--- src/sections/Section3.js
+++ src/sections/Section3.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+const styles = {
+    textArea: {
+        color: '#606462',
+        fontSize: '20px',
+        position: 'relative',
+        display: 'flex',
+        float: 'left',
+        
+        
+
+    }
+}
+
+
+
 function Section3() {
     return (
         <div className="section">
-            
+            <h1 style={styles.textArea}><i>"When the workload is too much,
+                you'll need some effective back up. That's where ReWORX comes in."</i>
+            </h1>
+
         </div>
     )
 }
