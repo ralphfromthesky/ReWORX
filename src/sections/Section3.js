d6dd28ee44eec547603dfe72492dc3d6b396ff8c
ralph <smmvaralph@yahoo.com>
2021-11-28 06:44:27 -0800

    update6

--- src/sections/Section3.js
+++ src/sections/Section3.js
@@ -2,14 +2,30 @@ import React from 'react';
 
 const styles = {
     textArea: {
-        position: 'absolute',
+        display: 'flex',
         color: '#606462',
         fontSize: '25px',
         textShadow: '2px 2px 4px white',
+        width: '50%',
+        padding: '20px'
+    },
 
+    textArea2: {
+        position: 'absolute',
+        fontSize: '20px',
+        color: '#178260',
+        textShadow: '2px white',
+        top: '170%',
+        left: '45%'
+        
+    },
 
-
-
+    textArea3: {
+        position: 'absolute',
+        textShadow: '1 px white',
+        color: '606462',
+        fontSize: '15px',
+        left: '70%',
 
 
     }
@@ -24,6 +40,8 @@ function Section3() {
             <h1 style={styles.textArea}><i>"When the workload is too much,
                 you'll need some effective back up. That's where ReWORX comes in."</i>
             </h1>
+            <h2 style={styles.textArea2}>- Ryan Gerome Regis, Ralph Santolorin and Ronald Velarde</h2>
+            <p style={styles.textArea3}>ReWORX Founder and Co-Founder</p>
 
         </div>
     )
