14e255f0e4f2f8c845ebb5bf991db4982da7f4a7
ralph <smmvaralph@yahoo.com>
2021-11-29 08:30:30 -0800

    update10

--- src/sections/Section1.js
+++ src/sections/Section1.js
@@ -1,56 +1,69 @@
 import React from 'react';
-
+import { Typography, Box, ThemeProvider, Button } from '@mui/material';
+import customTheme from './components/Theme';
 
 const styles = {
-    textArea: {
-        color: 'white',
-        fontSize: '27px',
-        position: 'absolute',
-        top: '20%',
-        left: '10%'
+    container: {
+        width: '100%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'stretch'
     },
-    button: {
-        padding: '5px',
-        backgroundColor: '#178260',
-        fontSize: '20px',
-        borderRadius: '20px',
-        color: 'white',
-        position: 'absolute',
-        top: '110%',
-        left: '100%',
-        borderColor: 'silver',
+    firstClass: {
+        width: '40%',
+        height: '40%'
+    },
+    secondClass: {
+        width: '40%',
+        height: '40%'
+    },
+    thirdClass: {
+        background: 'linear-gradient(to left, #9FDC7F 15%, #178260 65%)',
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent',
 
     }
-
 }
 
 
 
 function Section1() {
     return (
-<<<<<<< HEAD
-        <><div style={styles.textArea}>
+
+
+        <div className="section-1">
+            <ThemeProvider theme={customTheme}>
+                <Box sx={{ width: '50%', flexShrink: 0, padding: '3%' }}>
+                    <Typography variant='h1' color="info.light">
+                        Strategic Outsourcing, Re<span style={styles.thirdClass}>WORX</span>ed.
+                    </Typography>
+                </Box>
+                <Box sx={{width: '100%', display: 'flex', justifyContent: 'center', mt: '5%', borderRadius: '20px'}}>
+                    <Button variant='contained'>
+                    <Typography variant='body3'>
+                        Outsource With Us
+                    </Typography>
+                    </Button>
+                </Box>
+
+            </ThemeProvider>
+            {/* <div className={styles.container}>
+                <div className={styles.firstClass}>
+                    <h1>Strategic Outsourcing, ReWORXed.</h1>
+                </div>
+                <div className={styles.secondClass}>
+                    <button>Outsource With Us </button>
+                </div>
+            </div> */}
+        </div >
+
+    )
+}
+/* <div style={styles.textArea}>
             <h1>Strategic</h1>
             <h1>Outsourcing,</h1>
             <h1>ReWORXed.</h1>
-            <input type="button" value="Outsource with us ▼" style={styles.button}></input>
-=======
-        <div className="section-1">
-            <div>
-                <h1>Strategic Outsourcing, ReWORXed.</h1>
-            </div>
-            <div>
-                <button></button>
-            </div>
->>>>>>> 545a414a62822b80131985118d7ccfb60ec08fc6
-        </div>
-            <div className="section-1">
-                <div>
-                    <h1>ReWORX</h1>
-                    <p>Strategic Outsourcing, ReWORXed.</p>
-                </div>
-            </div></>
-    )
-}
-
+            <input type="button" value="Outsource with us ▼" style={styles.button}></input> */
 export default Section1;
--- src/sections/Section3.js
+++ src/sections/Section3.js
@@ -1,32 +1,44 @@
 import React from 'react';
 
 const styles = {
+  
     textArea: {
-       
+        
+        height: '60vh',
         display: 'flex',
+        paddingTop: '15%',
         color: '#606462',
-        fontSize: '40px',
+        fontSize: '25px',
         textShadow: '2px 2px 4px white',
         width: '65%',
-        padding: '20px',
-       
+        padding: '5%',
+      
+
     },
 
     textArea2: {
-      
-        fontSize: '22px',
+        
+        display: 'flex',
+        paddingRight: '10%',
+        textAlign: 'right',
+        justifyContent: 'right',
+        fontSize: '30px',
         color: '#178260',
         textShadow: '1px 1px 2px white',
-       
-        
+
+
     },
 
     textArea3: {
        
+        display: 'flex',
+        paddingRight: '10%',
+        textAlign: 'right',
+        justifyContent: 'right',
         textShadow: '1px 1px 2px white',
         color: '606462',
-        fontSize: '17px',
-       
+        fontSize: '25px',
+
 
     }
 }
@@ -35,25 +47,30 @@ const styles = {
 
 function Section3() {
     return (
-        <div id="section-3" className="section section-3">
-<<<<<<< HEAD
+        <div id="section-3" className="section section-3"> 
 
-            <h1 style={styles.textArea}><i>"When the workload is too much,
+            {/* <h1 style={styles.textArea}><i>"When the workload is too much,
                 you'll need some effective back up. That's where ReWORX comes in."</i>
             </h1>
             <h2 style={styles.textArea2}>- Ryan Gerome Regis, Ralph Santolorin and Ronald Velarde</h2>
-            <p style={styles.textArea3}>ReWORX Founder and Co-Founder</p>
+            <p style={styles.textArea3}>ReWORX Founder and Co-Founder</p> */}
 
-        
-            
-=======
-            <div>
-                <h1></h1>
+
+
+            <div style={styles.textArea}>
+                <h1>
+                    "When the workload is too much,
+                    you'll need some effective back up. That's where ReWORX comes in."
+                </h1>
             </div>
             <div>
-                <p></p>
+                <div style={styles.textArea2}>
+                    - Ryan Gerome Regis, Ralph Santolorin and Ronald Velarde
+                </div>
+                <div style={styles.textArea3}>
+                    ReWORX Founder and Co-Founder
+                </div>
             </div>
->>>>>>> 545a414a62822b80131985118d7ccfb60ec08fc6
         </div>
     )
 }
